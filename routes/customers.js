fbd177796c2111fca73b3d998bd609b53520c0a3
shibli <a@b.com>
2018-11-13 14:28:22 +0600

    first commit

---
+++ routes/customers.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+let verifyToken = require('./verifytoken');
+/* GET customers listing. */
+router.get('/', function(req, res, next) {
+    res.send('respond with a resource');
+});
+/* GET customers listing without protection. */
+router.get('/data', verifyToken, function(req, res, next) {
+    let customerdata = [{
+            customerid: 1,
+            customername: 'Mahfuz Bappy'
+        },
+        {
+            customerid: 2,
+            customername: 'Shamim Uddin'
+        },
+        {
+            customerid: 3,
+            customername: 'Ishani Isha'
+        }
+    ];
+
+    res.json(customerdata);
+});
+module.exports = router;
